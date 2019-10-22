cf77ee18d83c555d05c0927e3a068c66a5f5302d
ietukz@gmail.com <ietukz@gmail.com>
2019-10-21 20:30:07 -0500

    add introductory python file

---
+++ c5T1.py
@@ -0,0 +1,248 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print ("hello world")
+
+
+# In[2]:
+
+
+import math
+
+
+# In[3]:
+
+
+math.sqrt(25)
+
+
+# In[4]:
+
+
+math.pi
+
+
+# In[5]:
+
+
+import math as mt
+
+
+# In[6]:
+
+
+mt.sqrt(25)
+
+
+# mt sqrt(25)
+
+# In[7]:
+
+
+get_ipython().run_line_magic('lsmagic', '')
+
+
+# In[8]:
+
+
+get_ipython().run_line_magic('pwd', '')
+
+
+# In[9]:
+
+
+get_ipython().run_line_magic('ls', '-la')
+
+
+# In[10]:
+
+
+from numpy import random
+
+
+# In[11]:
+
+
+import numpy as np
+
+
+# In[13]:
+
+
+# uniform random numbers in [0,1]
+dataOne = random.rand(5,5)
+
+
+# In[14]:
+
+
+dataOne
+
+
+# In[15]:
+
+
+np.mean(dataOne)
+
+
+# In[16]:
+
+
+import math as mt
+
+
+# In[17]:
+
+
+d = ave(5,5)
+
+
+# In[18]:
+
+
+# take sume of numbers
+dataTwo = [10, 15, 25]
+
+
+# In[19]:
+
+
+dataTwo
+
+
+# In[20]:
+
+
+sum = float(15) + float(25)
+
+
+# In[21]:
+
+
+print("The sum of 15 and 25 is: ", sum)
+
+
+# In[23]:
+
+
+mt.sqrt(50)
+
+
+# In[24]:
+
+
+np.mean(sum)
+
+
+# In[26]:
+
+
+print('I Love data Science')
+
+
+# In[27]:
+
+
+# variables
+variableName = 25
+variable2 = 30
+
+
+# In[28]:
+
+
+print(variableName, variable2)
+
+
+# In[29]:
+
+
+# data types
+variableName = 25
+
+
+# In[30]:
+
+
+type(variableName)
+
+
+# In[31]:
+
+
+variableName = 25.0
+
+
+# In[32]:
+
+
+type(variableName)
+
+
+# In[33]:
+
+
+varOne = 25
+
+varTwo = 25.0
+
+varThree = varOne + varTwo
+
+
+# In[34]:
+
+
+print(varThree)
+
+
+# In[36]:
+
+
+# cannot add a string charater to integer
+varOne = 25
+
+varTwo = 'Hello'
+
+varThree = varOne + varTwo
+
+
+# In[37]:
+
+
+#List is a data structure just like a df in R
+listOne = [1,2,3,4]
+
+
+# In[38]:
+
+
+print(listOne[1:3])
+
+
+# In[39]:
+
+
+#dictionaries are like lists, except that each element is a key-value pair
+tel = {'jack': 4098, 'sape': 4139}
+
+
+# In[40]:
+
+
+tel['jack']
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
